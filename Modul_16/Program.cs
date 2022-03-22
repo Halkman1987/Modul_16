3a3b4e6a2edf54a24e85e32a38b45be38808ec54
Halkman <halk-man@mail.ru>
2022-03-23 00:44:29 +0500

    16

--- Modul_16/Program.cs
+++ Modul_16/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul_16
+{
+    class Program
+    {
+        static void Main()
+        {
+
+        }
+    }
+
+}
---
+++ Modul_16_Tests/CalculatorTests.cs
@@ -0,0 +1,43 @@
+﻿using Modul_16;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul_16_Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void TestAdditional()
+        {
+            Calculator calc = new Calculator();
+            Assert.That(calc.Additional(4,9)==13);
+
+        }
+        [Test]
+        public void TestSubtraction()
+        {
+            Calculator calc = new Calculator();
+            Assert.That(calc.Subtraction(6,3)== 3);
+
+        }
+        [Test]
+        public void TestMiltiplication()
+        {
+            Calculator calc = new Calculator();
+            Assert.True(calc.Miltiplication(4,2) == 8);
+
+        }
+        [Test]
+        public void TestDivision()
+        {
+            Calculator calc = new Calculator();
+            Assert.That(calc.Division(10,2)==5);
+
+        }
+    }
+}
